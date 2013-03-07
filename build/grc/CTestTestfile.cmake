9396927cb4d4da3fea1fc1b1a00bc37c7d254c18
SDR Commandos <sdr@sdr.(none)>
2013-03-07 15:08:02 -0500

    premerge

--- build/grc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chris/workspace/GNURadio/gr-eecs/grc
-# Build directory: /home/chris/workspace/GNURadio/gr-eecs/build/grc
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
