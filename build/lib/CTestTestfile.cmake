6cc316e6931a42216a83f03034164abe7d74bde9
SDR Commandos <sdr@sdr.(none)>
2013-03-07 15:14:12 -0500

    del build

--- build/lib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sdr/workspace/GNURadio/lib
-# Build directory: /home/sdr/workspace/GNURadio/build/lib
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(test_eecs "/bin/sh" "/home/sdr/workspace/GNURadio/build/lib/test_eecs_test.sh")
--- build/python/qa_d_theta_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-export GR_DONT_LOAD_PREFS=1
-export srcdir=/home/sdr/workspace/GNURadio/python
-export PATH=/home/sdr/workspace/GNURadio/build/python:$PATH
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$DYLD_LIBRARY_PATH
-export DYLD_LIBRARY_PATH=$LD_LIBRARY_PATH:$DYLD_LIBRARY_PATH
-export PYTHONPATH=/home/sdr/workspace/GNURadio/build/swig:$PYTHONPATH
-/usr/bin/python2.7 /home/sdr/workspace/GNURadio/python/qa_d_theta.py 
