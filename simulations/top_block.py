16160e316d7aaa0b774265d22749fa81d04b01d6
SDR Commandos <sdr@sdr.(none)>
2013-03-26 01:01:57 -0400

    sims w/ noite

--- simulations/top_block.py
+++ simulations/top_block.py
@@ -2,10 +2,11 @@
 ##################################################
 # Gnuradio Python Flow Graph
 # Title: Top Block
-# Generated: Tue Mar 26 00:39:28 2013
+# Generated: Tue Mar 26 01:01:06 2013
 ##################################################
 
 from gnuradio import analog
+from gnuradio import blocks
 from gnuradio import eng_notation
 from gnuradio import gr
 from gnuradio.eng_option import eng_option
@@ -129,9 +130,13 @@ class top_block(grc_wxgui.top_block_gui):
 		self.gr_complex_to_real_0_0_0 = gr.complex_to_real(1)
 		self.gr_complex_to_real_0_0 = gr.complex_to_real(1)
 		self.gr_complex_to_real_0 = gr.complex_to_real(1)
-		self.eecs_x_corr_0 = eecs.x_corr(fc, samp_rate, 64)
+		self.eecs_x_corr_0 = eecs.x_corr(fc, samp_rate, 128)
 		self.eecs_phase_shifter_0 = eecs.phase_shifter(20*wl, wl/2, theta*math.pi/180, wl, 0)
+		self.blocks_add_xx_0_0 = blocks.add_vcc(1)
+		self.blocks_add_xx_0 = blocks.add_vcc(1)
 		self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_SIN_WAVE, fc, 1, 0)
+		self.analog_noise_source_x_0_0 = analog.noise_source_c(analog.GR_GAUSSIAN, .5, 0)
+		self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, .2, 0)
 
 		##################################################
 		# Connections
@@ -140,18 +145,10 @@ class top_block(grc_wxgui.top_block_gui):
 		self.connect((self.eecs_x_corr_0, 0), (self.gr_complex_to_real_0, 0))
 		self.connect((self.gr_complex_to_real_0_2, 0), (self.wxgui_scopesink2_0, 1))
 		self.connect((self.gr_complex_to_real_0, 0), (self.wxgui_scopesink2_0, 0))
-		self.connect((self.analog_sig_source_x_0, 0), (self.gr_throttle_0, 0))
 		self.connect((self.eecs_phase_shifter_0, 3), (self.eecs_x_corr_0, 3))
 		self.connect((self.eecs_phase_shifter_0, 2), (self.eecs_x_corr_0, 2))
-		self.connect((self.eecs_phase_shifter_0, 1), (self.eecs_x_corr_0, 1))
-		self.connect((self.eecs_phase_shifter_0, 0), (self.eecs_x_corr_0, 0))
-		self.connect((self.gr_throttle_0, 0), (self.eecs_phase_shifter_0, 0))
 		self.connect((self.gr_complex_to_real_0_0, 0), (self.wxgui_scopesink2_0_1, 1))
 		self.connect((self.gr_complex_to_real_0_1, 0), (self.wxgui_scopesink2_0_1, 0))
-		self.connect((self.eecs_x_corr_0, 3), (self.gr_complex_to_real_0_0, 0))
-		self.connect((self.eecs_x_corr_0, 2), (self.gr_complex_to_real_0_1, 0))
-		self.connect((self.eecs_phase_shifter_0, 0), (self.gr_complex_to_real_0_2_0, 0))
-		self.connect((self.eecs_phase_shifter_0, 1), (self.gr_complex_to_real_0_1_0, 0))
 		self.connect((self.eecs_phase_shifter_0, 3), (self.gr_complex_to_real_0_0_0, 0))
 		self.connect((self.eecs_phase_shifter_0, 2), (self.gr_complex_to_real_0_3, 0))
 		self.connect((self.gr_complex_to_real_0_0_0, 0), (self.wxgui_scopesink2_0_0, 3))
@@ -160,6 +157,18 @@ class top_block(grc_wxgui.top_block_gui):
 		self.connect((self.gr_complex_to_real_0_2_0, 0), (self.wxgui_scopesink2_0_0, 0))
 		self.connect((self.gr_complex_to_real_0_1_0, 0), (self.wxgui_scopesink2_0_1_0, 1))
 		self.connect((self.gr_complex_to_real_0_3, 0), (self.wxgui_scopesink2_0_1_0, 0))
+		self.connect((self.gr_throttle_0, 0), (self.eecs_phase_shifter_0, 0))
+		self.connect((self.analog_noise_source_x_0, 0), (self.blocks_add_xx_0, 1))
+		self.connect((self.analog_noise_source_x_0_0, 0), (self.blocks_add_xx_0_0, 1))
+		self.connect((self.analog_sig_source_x_0, 0), (self.gr_throttle_0, 0))
+		self.connect((self.blocks_add_xx_0_0, 0), (self.eecs_x_corr_0, 1))
+		self.connect((self.blocks_add_xx_0, 0), (self.eecs_x_corr_0, 0))
+		self.connect((self.eecs_x_corr_0, 3), (self.gr_complex_to_real_0_0, 0))
+		self.connect((self.eecs_phase_shifter_0, 0), (self.blocks_add_xx_0, 0))
+		self.connect((self.eecs_phase_shifter_0, 1), (self.blocks_add_xx_0_0, 0))
+		self.connect((self.eecs_x_corr_0, 2), (self.gr_complex_to_real_0_1, 0))
+		self.connect((self.blocks_add_xx_0, 0), (self.gr_complex_to_real_0_2_0, 0))
+		self.connect((self.blocks_add_xx_0_0, 0), (self.gr_complex_to_real_0_1_0, 0))
 
 
 	def get_fc(self):
@@ -168,9 +177,9 @@ class top_block(grc_wxgui.top_block_gui):
 	def set_fc(self, fc):
 		self.fc = fc
 		self.set_wl(300e6/self.fc)
+		self.eecs_x_corr_0.set_freq(self.fc)
 		self.analog_sig_source_x_0.set_frequency(self.fc)
 		self.set_samp_rate(self.fc*20)
-		self.eecs_x_corr_0.set_freq(self.fc)
 
 	def get_wl(self):
 		return self.wl
@@ -185,22 +194,22 @@ class top_block(grc_wxgui.top_block_gui):
 
 	def set_theta(self, theta):
 		self.theta = theta
-		self.eecs_phase_shifter_0.set_theta(self.theta*math.pi/180)
 		self._theta_slider.set_value(self.theta)
 		self._theta_text_box.set_value(self.theta)
+		self.eecs_phase_shifter_0.set_theta(self.theta*math.pi/180)
 
 	def get_samp_rate(self):
 		return self.samp_rate
 
 	def set_samp_rate(self, samp_rate):
 		self.samp_rate = samp_rate
-		self.gr_throttle_0.set_sample_rate(self.samp_rate)
-		self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
 		self.wxgui_scopesink2_0_1.set_sample_rate(self.samp_rate)
 		self.wxgui_scopesink2_0.set_sample_rate(self.samp_rate)
 		self.wxgui_scopesink2_0_1_0.set_sample_rate(self.samp_rate)
-		self.eecs_x_corr_0.set_sampRate(self.samp_rate)
+		self.gr_throttle_0.set_sample_rate(self.samp_rate)
 		self.wxgui_scopesink2_0_0.set_sample_rate(self.samp_rate)
+		self.eecs_x_corr_0.set_sampRate(self.samp_rate)
+		self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
 
 if __name__ == '__main__':
 	parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
