54a411a679b51630e2a4fd8803e090610bab6ae1
Chris Acosta <acostaj@umich.edu>
2013-03-26 20:26:12 -0400

    Fixed the Cross correlation block, works well now as a 2x1 we can cascade

--- lib/x_corr_impl.cc
+++ lib/x_corr_impl.cc
@@ -44,8 +44,8 @@ namespace gr {
 							float sampRate,
 							int nSamples)
       : gr_sync_block("x_corr",
-		      gr_make_io_signature(4, 4, sizeof (gr_complex)),
-		      gr_make_io_signature(4, 4, sizeof (gr_complex))),
+		      gr_make_io_signature(2, 2, sizeof (gr_complex)),
+		      gr_make_io_signature(1, 1, sizeof (gr_complex))),
 		      p_freq(freq), p_sampRate(sampRate), p_nSamples(nSamples)
 		{
 		set_history(p_nSamples);
@@ -60,32 +60,18 @@ namespace gr {
     {
         const gr_complex *Sat1 = (const gr_complex *) input_items[0];
         const gr_complex *Sat2 = (const gr_complex *) input_items[1];
-        const gr_complex *Sat3 = (const gr_complex *) input_items[2];
-        const gr_complex *Sat4 = (const gr_complex *) input_items[3];
-		gr_complex *out1 = (gr_complex *) output_items[0];
-		gr_complex *out2 = (gr_complex *) output_items[1];
-		gr_complex *out3 = (gr_complex *) output_items[2];
-		gr_complex *out4 = (gr_complex *) output_items[3];
-		Sat1 += p_nSamples; Sat2 += p_nSamples; 
-		Sat3 += p_nSamples; Sat4 += p_nSamples;// ensure that sample range is valid.
+		gr_complex *out = (gr_complex *) output_items[0];
+
+		Sat1 += p_nSamples; Sat2 += p_nSamples;//ensure that sample range is valid.
 		
-
-		float xcor[2][p_nSamples];		
-
-		float theta[3];
+		float xcor[2*p_nSamples];
+		float theta;
 //////////		
-		xcorr(Sat1, Sat2, xcor[0]);
-		//xcorr(Sat2, Sat3, xcor[1]);
-		//xcorr(Sat3, Sat4, xcor[2]);
-		theta[0] =0;
-		theta[1] =findTheta(xcor[0]);
-		theta[2] =0;
-		theta[3] =0;
+		xcorr(Sat1, Sat2, xcor);
+		theta =findTheta(xcor);
+
 		for(int i = 0; i <noutput_items; i++){
-			out1[i] = Sat1[i];//*gr_complex(cos(theta[0]), sin(theta[0]));
-			out2[i] = Sat2[i]*gr_complex(cos(-1*theta[1]*M_PI/180), sin(-1*theta[1]*M_PI/180));
-			out3[i] = theta[0];//Sat3[i]*gr_complex(cos(theta[2]), sin(theta[2]));
-			out4[i] = theta[1];//Sat4[i]*gr_complex(cos(theta[3]), sin(theta[3]));	
+			out[i] = Sat1[i]+Sat2[i]*gr_complex(cos(-1*theta), sin(-1*theta));
 		}     
         
         return noutput_items;
@@ -98,15 +84,15 @@ namespace gr {
 			r1[i] = real(grc_r1[i]);
 			r2[i] = real(grc_r2[i]);
 		}
-		for(int i = 0; i < p_nSamples-1; i++){							// for i = 1:len
-			xout[i] = 0;												//   xcor12(i) = 0;
-			for (int j = 0; j <= i; j++){								//   for j=1:i
-				xout[i] = xout[i]+ r1[i-j]*r2[p_nSamples-j];					//     xcor12(i) = xcor12(i) + r1C(i-j+1)*r2(len-j+1);
+		for(int i = 0; i < p_nSamples-1; i++){							
+			xout[i] = 0;												
+			for (int j = 0; j <= i; j++){								
+				xout[i] = xout[i]+ r1[i-j]*r2[p_nSamples-j];					
 		}}	
-		for(int i = 0; i < p_nSamples-2; i++){							//for i = 0:len-1
-			xout[p_nSamples+i] = 0;										//   xcor12(len+i) = 0;
-			for(int j = 0; j < (p_nSamples-1-i); j++){					//   for j = 1:len-i
-			  xout[p_nSamples+i] += r1[p_nSamples-1 +i]*r2[j];  		//      xcor12(len+i) = xcor12(len+i) + r1C(i+j)*r2(j);
+		for(int i = 0; i < p_nSamples-2; i++){							
+			xout[p_nSamples+i] = 0;										
+			for(int j = 0; j < (p_nSamples-1-i); j++){					
+			  xout[p_nSamples+i] += r1[p_nSamples-1 +i]*r2[j];  		
 					}}
 	}
 	
@@ -119,18 +105,17 @@ namespace gr {
 				max_val = sig[i];
 				max_index = i;
 		}}
-	return dp((max_index - p_nSamples)/p_sampRate*2*p_freq*180);
+	return dp((max_index - p_nSamples)/p_sampRate*2*M_PI*p_freq);
 	}
 	
 	float x_corr_impl::dp(float phase){
-		if (phase < -360 || phase == -360)
-			return dp(phase+360);
-		else if (phase > 360 || phase == 360)
-			return dp(phase -360);
+		if (phase < -2*M_PI || phase == -2*M_PI){
+			return dp(phase+2*M_PI);}
+		else if (phase > 2*M_PI || phase == 2*M_PI){
+			return dp(phase-2*M_PI);}
 		else
 			return(phase);
 	}
-
 
 	void x_corr_impl::set_freq(float freq)
 	{	p_freq = freq;	}
